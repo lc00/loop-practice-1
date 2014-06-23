a904523cfc154ff73aef42fea989cb36f73de0cb
lc00 <leannecheung00@gmail.com>
2014-06-23 12:01:06 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+for (var i=0; i<6; i++){
+	console.log(i * 100);
+}
+
+for (i=1; i<=64; i*=2){
+
+	console.log(i);
+}
+
+for(i=1; i<4; i+=1){
+	for(var j=0; j<3; j+=1)
+		console.log(i);
+}
+
+for(i=0; i<=10; i+=2){
+	console.log(i);
+}
+
+for(i=3; i<=15; i+=3){
+	console.log(i);
+}
+
+for(i=9; i>=0; i-=1){
+	console.log(i);
+}
+
+for(i=0; i<3; i+=1){
+	for(j=0; j<=3; j+=1){
+		console.log(j);
+	}
+}
